354a45d582217e25067c41f63052a85125310d5e
nikita <nilitagram@gmail.com>
2020-07-31 19:40:51 +0300

    Updates

---
+++ precache-manifest.08e862aa1a2dc82cb72e1f05c28e3fc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0b957f7e92da3c9ddbd63632c4db5e",
+    "url": "/aviasales-tickets/index.html"
+  },
+  {
+    "revision": "51cbff2daa44fa552205",
+    "url": "/aviasales-tickets/static/css/main.ecd4c370.chunk.css"
+  },
+  {
+    "revision": "159720b93ceced5581e0",
+    "url": "/aviasales-tickets/static/js/2.32e05233.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aviasales-tickets/static/js/2.32e05233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51cbff2daa44fa552205",
+    "url": "/aviasales-tickets/static/js/main.ae350a71.chunk.js"
+  },
+  {
+    "revision": "aa97381c89b40aa696ba",
+    "url": "/aviasales-tickets/static/js/runtime-main.d1a60e8f.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales-tickets/static/media/logo.0c089234.svg"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/aviasales-tickets/static/media/shape-icon.68ca57dc.svg"
+  }
+]);
